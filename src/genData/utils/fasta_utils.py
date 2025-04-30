be25aac0c0364e5b516b67dd062ff1db6d59ead0
Katarina Gresova <514001@mail.muni.cz>
2025-04-30 14:14:41 +0000

    use python comprehensions to load data, convert to uppercase

--- src/genData/utils/fasta_utils.py
+++ src/genData/utils/fasta_utils.py
@@ -1,10 +1,7 @@
 from Bio import SeqIO, SeqRecord, Seq
 
 def read_fasta(fasta_file):
-    sequences = []
-    for record in SeqIO.parse(fasta_file, 'fasta'):
-        sequences.append(str(record.seq))
-    return sequences
+    return [str(record.seq).upper() for record in SeqIO.parse(fasta_file, 'fasta')]
 
 def write_fasta(sequences, output_file):
     records = [SeqRecord.SeqRecord(Seq.Seq(seq), id=f'seq_{i}') for i, seq in enumerate(sequences)]
